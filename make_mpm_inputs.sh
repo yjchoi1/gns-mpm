10b5485aee1178ca55c2596c26f70c77eff06692
Yongjin Choi <baagee@c196-011.frontera.tacc.utexas.edu>
2022-09-25 10:24:35 -0500

    trivial

--- make_mpm_inputs.sh
+++ make_mpm_inputs.sh
@@ -1,6 +1,6 @@
-python3 mpm_input_gen/make_mpm_input.py --x_bounds 0.0 1.0 --y_bounds 0.0 1.0 --x_range 0.0 0.3 --y_range 0.0 0.3
+python3 mpm_input_gen/make_mpm_input.py --x_bounds 0.0 2.0 --y_bounds 0.0 2.0 --x_range 0.8 1.2 --y_range 0.0 0.6
 
-MPM_DIR="./mpm/abc"
+MPM_DIR="./mpm/mpm-small-test7"
 mv particles.txt "${MPM_DIR}"
 mv entity_sets.json "${MPM_DIR}"
 mv mesh.txt "${MPM_DIR}"
--- run_mpm.sh
+++ run_mpm.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-#SBATCH -J test-s6-2
-#SBATCH -o test-s6-2.o%j
-#SBATCH -e test-s6-2.e%j
+#SBATCH -J test-s7
+#SBATCH -o test-s7.o%j
+#SBATCH -e test-s7.e%j
 #SBATCH -n 1
 #SBATCH -N 1
 #SBATCH -p small
-#SBATCH -t 02:00:00
+#SBATCH -t 07:00:00
 
 module reset
 
-export MPM_DIR="./mpm/mpm-small-test6-2/"
+export MPM_DIR="./mpm/mpm-small-test7/"
 echo "${MPM_DIR}"
 
 /work/08264/baagee/frontera/mpm/build/mpm -i /mpm.json -f "${MPM_DIR}"
