13766f06d453d6fe8557c0cea05065d1a63b97ff
Yongjin Choi <baagee@login1.ls6.tacc.utexas.edu>
2022-10-10 09:46:11 -0500

    trivial

--- rollout-2d.sh
+++ rollout-2d.sh
@@ -7,13 +7,17 @@ cd gns
 # workdir
 export WORK_DIR="../gns-data"
 export DATASET_NAME="sand-2d-small-r300"
-export STEPS=15760000
+export STEPS=20000000
+export OUTPUT_TAG="test0-2"
+export TRAJECTORY_ID="0"
 
 # Generate test rollouts.
-python3 -m gns.train --mode='rollout' --data_path="${WORK_DIR}/datasets/${DATASET_NAME}/" --model_path="${WORK_DIR}/models/${DATASET_NAME}/" --model_file="model-${STEPS}.pt" --train_state_file="train_state-${STEPS}.pt" --output_path="${WORK_DIR}/rollouts/${DATASET_NAME}"
+python3 -m gns.train --mode='rollout' --data_path="${WORK_DIR}/datasets/${DATASET_NAME}/" --model_path="${WORK_DIR}/models/${DATASET_NAME}/" --model_file="model-${STEPS}.pt" --train_state_file="train_state-${STEPS}.pt" --output_path="${WORK_DIR}/rollouts/${DATASET_NAME}" --rollout_tag=${OUTPUT_TAG}
 
 # Render rollout
-python3 -m gns.render_rollout --rollout_path="${WORK_DIR}/rollouts/${DATASET_NAME}/rollout_0.pkl"
+python3 -m gns.render_rollout --rollout_path="${WORK_DIR}/rollouts/${DATASET_NAME}/rollout_${OUTPUT_TAG}_${TRAJECTORY_ID}.pkl" --dataset_name=${DATASET_NAME} --output_tag=${OUTPUT_TAG}_${TRAJECTORY_ID}
 
+# Make plots for normalized runout and energy evolution	
 cd ..
-
+export WORK_DIR="gns-data"
+python3 utils/rollout_analysis.py --rollout_path ${WORK_DIR}/rollouts/${DATASET_NAME} --rollout_filename  rollout_${OUTPUT_TAG}_${TRAJECTORY_ID} --output_percentile 100
--- run_mpm.sh
+++ run_mpm.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-#SBATCH -J test-s7
-#SBATCH -o test-s7.o%j
-#SBATCH -e test-s7.e%j
+#SBATCH -J train-large9
+#SBATCH -o train-large9.o%j
+#SBATCH -e train-large9.e%j
 #SBATCH -n 1
 #SBATCH -N 1
 #SBATCH -p small
-#SBATCH -t 07:00:00
+#SBATCH -t 03:00:00
 
 module reset
 
-export MPM_DIR="./mpm/mpm-small-test7/"
+export MPM_DIR="./mpm/mpm-large-train9/"
 echo "${MPM_DIR}"
 
 /work/08264/baagee/frontera/mpm/build/mpm -i /mpm.json -f "${MPM_DIR}"
