8880f8e2de31fea52d4a0ea90ea6bb23aae37d10
Yongjin Choi <baagee@c196-011.frontera.tacc.utexas.edu>
2022-10-28 18:37:27 -0500

    seperate mpm.json input for each simulation condition

--- run_mpm.sh
+++ run_mpm.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
-#SBATCH -J train-large31
-#SBATCH -o train-large31.o%j
-#SBATCH -e train-large31.e%j
+#SBATCH -J mpm-6k-train0
+#SBATCH -o mpm-6k-train0.o%j
+#SBATCH -e mpm-6k-train0.e%j
 #SBATCH -n 1
 #SBATCH -N 1
-#SBATCH -p small
-#SBATCH -t 12:00:00
+#SBATCH -p skx-normal
+#SBATCH -t 8:00:00
+#SBATCH -A Material-Point-Metho
 
 module reset
 
-export MPM_DIR="./mpm/mpm-small-train0/"
+export MPM_DIR="./mpm/mpm-6k-train0/"
 echo "${MPM_DIR}"
 
-#/work/08264/baagee/frontera/mpm/build/mpm -i /mpm-initial.json -f "${MPM_DIR}"
-/work/08264/baagee/frontera/mpm/build/mpm -i /mpm.json -f "${MPM_DIR}"
+/work/08264/baagee/frontera/mpm/build/mpm -i /mpm_initial_stress.json -f "${MPM_DIR}"
+#/work/08264/baagee/frontera/mpm/build/mpm -i /mpm-initial-vel.json -f "${MPM_DIR}"
+#/work/08264/baagee/frontera/mpm/build/mpm -i /mpm_resume_initial_vel.json -f "${MPM_DIR}"
+
 #~/Documents/mpm/build/mpm -i /mpm.json -f "${MPM_DIR}"
