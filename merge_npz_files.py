9234e35e96f417ffc25d902f38a11545808fb0f6
baagee <yjchoi9212@gmail.com>
2023-01-03 16:57:29 -0600

    add 3d case

--- merge_npz_files.py
+++ merge_npz_files.py
@@ -2,26 +2,31 @@ import numpy as np
 import json
 
 # Inputs
-bounds = [[-1.0225332856518696, -1.0, -1.0370372322163504], [46.62253328565188, 1.0, 5.1939591703816586]]
-sequence_length = int(20)
-default_connectivity_radius = 10.0
+bounds = [[0, 1], [0, 1], [0, 1]]
+sequence_length = int(420)
+default_connectivity_radius = 0.15
 dim = int(3)
-dt_mpm = None  # 0.0025
-mpm_cell_size = None  # [0.0125, 0.0125]
-nparticles_per_cell = None  # int(16)
-dt_gns = 0.01
+dt_mpm = 0.0025  # 0.0025
+mpm_cell_size = [0.1, 0.1, 0.1]  # [0.0125, 0.0125]
+nparticles_per_cell = 4*4*4  # int(16)
+dt_gns = 1.0  # 1.0 is default
 
-mpm_dir = "./gns-data/datasets/test_files" # "./mpm"
-data_case = "test" # "mpm-9k-train"
+mpm_dir = "./mpm"  # "./mpm"
+data_case = "3dsand_train"  # "mpm-9k-train"
 # data_tags = [str(i) for i in np.arange(0, 24)] + [str(25), str(26)]
-data_tags = np.arange(1, 21, 1)
-save_name = "train-test"
+data_tags = range(0, 60)
+save_name = "3dsand_train_merged"
 
 
 trajectories = {}
-running_sum = dict(velocity_x=0, velocity_y=0, acceleration_x=0, acceleration_y=0)
-running_sumsq = dict(velocity_x=0, velocity_y=0, acceleration_x=0, acceleration_y=0)
-running_count = dict(velocity_x=0, velocity_y=0, acceleration_x=0, acceleration_y=0)
+if dim == 2:
+    running_sum = dict(velocity_x=0, velocity_y=0, acceleration_x=0, acceleration_y=0)
+    running_sumsq = dict(velocity_x=0, velocity_y=0, acceleration_x=0, acceleration_y=0)
+    running_count = dict(velocity_x=0, velocity_y=0, acceleration_x=0, acceleration_y=0)
+if dim == 3:
+    running_sum = dict(velocity_x=0, velocity_y=0, velocity_z=0, acceleration_x=0, acceleration_y=0, acceleration_z=0)
+    running_sumsq = dict(velocity_x=0, velocity_y=0, velocity_z=0, acceleration_x=0, acceleration_y=0, acceleration_z=0)
+    running_count = dict(velocity_x=0, velocity_y=0, velocity_z=0, acceleration_x=0, acceleration_y=0, acceleration_z=0)
 aggregated_positions = []
 aggregated_velocities = []
 aggregated_accelerations = []
@@ -30,7 +35,7 @@ data_names = []
 for id in data_tags:
     data_name = f"{data_case}{id}"
     data_names.append(data_name)
-    npz_path = f"{mpm_dir}/{data_name}.npz"  # f"{mpm_dir}/{data_name}/{data_name}.npz"
+    npz_path = f"{mpm_dir}/{data_name}/{data_name}.npz"  # f"{mpm_dir}/{data_name}/{data_name}.npz"
     data = np.load(npz_path, allow_pickle=True)
     for simulation_id, trajectory in data.items():
         trajectories[f"simulation_trajectory_{id}"] = (trajectory)
@@ -65,16 +70,32 @@ concat_velocities = np.concatenate(aggregated_velocities)
 concat_accelerations = np.concatenate(aggregated_accelerations)
 
 # Compute statistics
-statistics = {
-    "mean_velocity_x": np.mean(concat_velocities[:, 0]),
-    "mean_velocity_y": np.mean(concat_velocities[:, 1]),
-    "std_velocity_x": np.std(concat_velocities[:, 0]),
-    "std_velocity_y": np.std(concat_velocities[:, 1]),
-    "mean_accel_x": np.mean(concat_accelerations[:, 0]),
-    "mean_accel_y": np.mean(concat_accelerations[:, 1]),
-    "std_accel_x": np.std(concat_accelerations[:, 0]),
-    "std_accel_y": np.std(concat_accelerations[:, 1])
-}
+if dim == 2:
+    statistics = {
+        "mean_velocity_x": np.mean(concat_velocities[:, 0]),
+        "mean_velocity_y": np.mean(concat_velocities[:, 1]),
+        "std_velocity_x": np.std(concat_velocities[:, 0]),
+        "std_velocity_y": np.std(concat_velocities[:, 1]),
+        "mean_accel_x": np.mean(concat_accelerations[:, 0]),
+        "mean_accel_y": np.mean(concat_accelerations[:, 1]),
+        "std_accel_x": np.std(concat_accelerations[:, 0]),
+        "std_accel_y": np.std(concat_accelerations[:, 1])
+    }
+if dim == 3:
+    statistics = {
+        "mean_velocity_x": np.mean(concat_velocities[:, 0]),
+        "mean_velocity_y": np.mean(concat_velocities[:, 1]),
+        "mean_velocity_z": np.mean(concat_velocities[:, 2]),
+        "std_velocity_x": np.std(concat_velocities[:, 0]),
+        "std_velocity_y": np.std(concat_velocities[:, 1]),
+        "std_velocity_z": np.std(concat_velocities[:, 2]),
+        "mean_accel_x": np.mean(concat_accelerations[:, 0]),
+        "mean_accel_y": np.mean(concat_accelerations[:, 1]),
+        "mean_accel_z": np.mean(concat_accelerations[:, 2]),
+        "std_accel_x": np.std(concat_accelerations[:, 0]),
+        "std_accel_y": np.std(concat_accelerations[:, 1]),
+        "std_accel_z": np.std(concat_accelerations[:, 2])
+    }
 
 # Print statistics
 for key, value in statistics.items():
@@ -85,28 +106,46 @@ np.savez_compressed(f"{save_name}.npz", **trajectories)
 print(f"npz saved at: ./{save_name}.npz")
 
 # Save metadata.json
-metadata = {
-    "bounds": bounds,
-    "sequence_length": sequence_length,
-    "default_connectivity_radius": default_connectivity_radius,
-    "dim": dim,
-    "dt": dt_mpm,
-    "vel_mean": [statistics["mean_velocity_x"], statistics["mean_velocity_y"]],
-    "vel_std": [statistics["std_velocity_x"], statistics["std_velocity_y"]],
-    "acc_mean": [statistics["mean_accel_x"], statistics["mean_accel_y"]],
-    "acc_std": [statistics["std_accel_x"], statistics["std_accel_y"]],
-    "mpm_cell_size": mpm_cell_size,
-    "nparticles_per_cell": nparticles_per_cell,
-    "data_names": data_names
-}
+if dim == 2:
+    metadata = {
+        "bounds": bounds,
+        "sequence_length": sequence_length,
+        "default_connectivity_radius": default_connectivity_radius,
+        "boundary_augment": 1.0,
+        "dim": dim,
+        "dt": dt_mpm,
+        "vel_mean": [statistics["mean_velocity_x"], statistics["mean_velocity_y"]],
+        "vel_std": [statistics["std_velocity_x"], statistics["std_velocity_y"]],
+        "acc_mean": [statistics["mean_accel_x"], statistics["mean_accel_y"]],
+        "acc_std": [statistics["std_accel_x"], statistics["std_accel_y"]],
+        "mpm_cell_size": mpm_cell_size,
+        "nparticles_per_cell": nparticles_per_cell,
+        "data_names": data_names
+    }
+if dim == 3:
+    metadata = {
+        "bounds": bounds,
+        "sequence_length": sequence_length,
+        "default_connectivity_radius": default_connectivity_radius,
+        "boundary_augment": 1.0,
+        "dim": dim,
+        "dt": dt_mpm,
+        "vel_mean": [statistics["mean_velocity_x"], statistics["mean_velocity_y"], statistics["mean_velocity_z"]],
+        "vel_std": [statistics["std_velocity_x"], statistics["std_velocity_y"], statistics["std_velocity_z"]],
+        "acc_mean": [statistics["mean_accel_x"], statistics["mean_accel_y"], statistics["mean_accel_z"]],
+        "acc_std": [statistics["std_accel_x"], statistics["std_accel_y"], statistics["std_accel_z"]],
+        "mpm_cell_size": mpm_cell_size,
+        "nparticles_per_cell": nparticles_per_cell,
+        "data_names": data_names
+    }
 
 with open(f"metadata-{save_name}.json", "w") as fp:
     json.dump(metadata, fp)
 print(f"metadata saved at: ./{save_name}.json")
 
 
-# See npz
-data = np.load('train.npz', allow_pickle=True)
-for simulation_id, trajectory in data.items():
-    print(simulation_id)
-    print(trajectory)
+# # See npz
+# data = np.load('train.npz', allow_pickle=True)
+# for simulation_id, trajectory in data.items():
+#     print(simulation_id)
+#     print(trajectory)
