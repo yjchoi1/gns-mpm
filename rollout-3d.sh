0b90894bb3a82c98464835d6b69dd26682f3d994
baagee <yjchoi9212@gmail.com>
2023-01-17 23:16:12 -0600

    update render util to have 3d animation function

--- rollout-3d.sh
+++ rollout-3d.sh
@@ -3,12 +3,12 @@
 set -e
 
 # workdir
-export DATASET_NAME="sand-small-r300-400step_serial"
+export DATASET_NAME="3dsand-r010"
 #for ((STEPS=10000000; STEPS<14000000; STEPS+=1000000))
-for STEPS in 15270000
+for STEPS in 4850000
 do
 #export STEPS=15270000
-export OUTPUT_TAG="test5-3"
+export OUTPUT_TAG="test0"
 export TRAJECTORY_ID="0"
 
 ## Change test.npz that is used to make rollout to the specified `test_{OUTPUT_TAG}.npz which you want to make rollout
@@ -16,26 +16,27 @@ export WORK_DIR="gns-data"
 if test -f "${WORK_DIR}/datasets/${DATASET_NAME}/test.npz"
  then rm gns-data/datasets/${DATASET_NAME}/test.npz
 fi
-cp ${WORK_DIR}/datasets/${DATASET_NAME}/mpm-small-${OUTPUT_TAG}.npz ${WORK_DIR}/datasets/${DATASET_NAME}/test.npz
+cp ${WORK_DIR}/datasets/${DATASET_NAME}/3dsand_${OUTPUT_TAG}.npz ${WORK_DIR}/datasets/${DATASET_NAME}/test.npz
 
-cd gns
-# Generate test rollouts.
-export WORK_DIR="../gns-data"
-python3 -m gns.train --mode='rollout' \
---data_path="${WORK_DIR}/datasets/${DATASET_NAME}/" \
---model_path="${WORK_DIR}/models/${DATASET_NAME}/" \
---model_file="model-${STEPS}.pt" --train_state_file="train_state-${STEPS}.pt" \
---output_path="${WORK_DIR}/rollouts/${DATASET_NAME}" \
---rollout_tag=${OUTPUT_TAG} --rollout_step=${STEPS} \
---batch_size=1
-cd ..
+cd gns-parallel
+## Generate test rollouts.
+#export WORK_DIR="../gns-data"
+#python3 -m gns.train \
+#--mode='rollout' \
+#--data_path="${WORK_DIR}/datasets/${DATASET_NAME}/" \
+#--model_path="${WORK_DIR}/models/${DATASET_NAME}/" \
+#--model_file="model-${STEPS}.pt" \
+#--train_state_file="train_state-${STEPS}.pt" \
+#--output_path="${WORK_DIR}/rollouts/${DATASET_NAME}" \
+#--rollout_tag=${OUTPUT_TAG} --rollout_step=${STEPS}
 
 # Render rollout
-export WORK_DIR="gns-data"
-python3 utils/render_rollout.py \
+export WORK_DIR="../gns-data"
+python3 ../utils/render_rollout.py \
 --rollout_path="${WORK_DIR}/rollouts/${DATASET_NAME}" \
 --rollout_filename="rollout_${OUTPUT_TAG}_${TRAJECTORY_ID}_step${STEPS}" \
---output_filename="rollout_step${STEPS}_${OUTPUT_TAG}_${TRAJECTORY_ID}"
+--output_filename="rollout_step${STEPS}_${OUTPUT_TAG}_${TRAJECTORY_ID}" \
+--dims=3 \
 
 ## Make plots for normalized runout and energy evolution
 #export WORK_DIR="gns-data"
--- utils/render_rollout.py
+++ utils/render_rollout.py
@@ -41,6 +41,7 @@ flags.DEFINE_string("rollout_filename", None, help="The name of the rollout to r
 flags.DEFINE_string("output_filename", None, help="The name of output animation to save, e.g., rollout_test0-2")
 flags.DEFINE_integer("step_stride", 3, help="Stride of steps to skip.")
 flags.DEFINE_boolean("block_on_show", True, help="For test purposes.")
+flags.DEFINE_integer("dims", 2, help="dims")
 
 FLAGS = flags.FLAGS
 
@@ -61,49 +62,94 @@ def main(unused_argv):
     with open(f"{FLAGS.rollout_path}/{FLAGS.rollout_filename}.pkl", "rb") as file:
         rollout_data = pickle.load(file)
 
-    fig, axes = plt.subplots(1, 2, figsize=(10, 5))
-    fig.suptitle(rollout_data["loss"].cpu().numpy())
+    if FLAGS.dims == 2:
+        fig, axes = plt.subplots(1, 2, figsize=(10, 5))
+        fig.suptitle(rollout_data["loss"].cpu().numpy())
 
-    plot_info = []
-    for ax_i, (label, rollout_field) in enumerate(
-            [("Reality", "ground_truth_rollout"),
-             ("GNS", "predicted_rollout")]):
-        # Append the initial positions to get the full trajectory.
-        trajectory = np.concatenate([
-            rollout_data["initial_positions"],
-            rollout_data[rollout_field]], axis=0)
-        ax = axes[ax_i]
-        ax.set_title(label)
-        bounds = rollout_data["metadata"]["bounds"]
-        ax.set_xlim(bounds[0][0], bounds[0][1])
-        ax.set_ylim(bounds[1][0], bounds[1][1])
-        ax.set_xticks([])
-        ax.set_yticks([])
-        ax.set_aspect(1.)
-        ax.grid(visible=True)
-        points = {
-            particle_type: ax.plot([], [], "o", ms=1.0, color=color)[0]
-            for particle_type, color in TYPE_TO_COLOR.items()}
-        plot_info.append((ax, trajectory, points))
+        plot_info = []
+        for ax_i, (label, rollout_field) in enumerate(
+                [("Reality", "ground_truth_rollout"),
+                 ("GNS", "predicted_rollout")]):
+            # Append the initial positions to get the full trajectory.
+            trajectory = np.concatenate([
+                rollout_data["initial_positions"],
+                rollout_data[rollout_field]], axis=0)
+            ax = axes[ax_i]
+            ax.set_title(label)
+            bounds = rollout_data["metadata"]["bounds"]
+            ax.set_xlim(bounds[0][0], bounds[0][1])
+            ax.set_ylim(bounds[1][0], bounds[1][1])
+            ax.set_xticks([])
+            ax.set_yticks([])
+            ax.set_aspect(1.)
+            ax.grid(visible=True)
+            points = {
+                particle_type: ax.plot([], [], "o", ms=1.0, color=color)[0]
+                for particle_type, color in TYPE_TO_COLOR.items()}
+            plot_info.append((ax, trajectory, points))
 
-    num_steps = trajectory.shape[0]
+        num_steps = trajectory.shape[0]
 
-    def update(step_i):
-        outputs = []
-        for _, trajectory, points in plot_info:
-            for particle_type, line in points.items():
-                mask = rollout_data["particle_types"] == particle_type
-                line.set_data(trajectory[step_i, mask, 0],
-                              trajectory[step_i, mask, 1])
-                outputs.append(line)
-        return outputs
+        def update(step_i):
+            outputs = []
+            for _, trajectory, points in plot_info:
+                for particle_type, line in points.items():
+                    mask = rollout_data["particle_types"] == particle_type
+                    line.set_data(trajectory[step_i, mask, 0],
+                                  trajectory[step_i, mask, 1])
+                    outputs.append(line)
+            return outputs
 
-    unused_animation = animation.FuncAnimation(
-        fig, update,
-        frames=np.arange(0, num_steps, FLAGS.step_stride), interval=10)
+        unused_animation = animation.FuncAnimation(
+            fig, update,
+            frames=np.arange(0, num_steps, FLAGS.step_stride), interval=10)
 
-    unused_animation.save(f'{FLAGS.rollout_path}/{FLAGS.output_filename}.gif', dpi=100, fps=30, writer='imagemagick')
-    # plt.show(block=FLAGS.block_on_show)
+        unused_animation.save(f'{FLAGS.rollout_path}/{FLAGS.output_filename}.gif', dpi=100, fps=30, writer='imagemagick')
+        # plt.show(block=FLAGS.block_on_show)
+
+    elif FLAGS.dims == 3:
+
+        # get rollouts from .pkl file
+        trajectory = {}
+        rollout_fields = ["ground_truth_rollout", "predicted_rollout"]
+        for rollout_field in rollout_fields:
+            trajectory[rollout_field] = np.concatenate(
+                [rollout_data["initial_positions"], rollout_data[rollout_field]], axis=0
+            )
+        num_steps = trajectory["ground_truth_rollout"].shape[0]
+
+        # init figures
+        fig = plt.figure()
+        ax1 = fig.add_subplot(1, 2, 1, projection='3d')
+        ax2 = fig.add_subplot(1, 2, 2, projection='3d')
+        axes = [ax1, ax2]
+
+        # fig creating function
+        def animate(i):
+            fig.clear()
+            xboundary = rollout_data["metadata"]["bounds"][0]
+            yboundary = rollout_data["metadata"]["bounds"][1]
+            zboundary = rollout_data["metadata"]["bounds"][2]
+
+            rollout_fields = ["ground_truth_rollout", "predicted_rollout"]
+
+            for j, rollout_field in enumerate(rollout_fields):
+                axes[j] = fig.add_subplot(1, 2, j+1, projection='3d', autoscale_on=False)
+                axes[j].set_xlim([float(xboundary[0]), float(xboundary[1])])
+                axes[j].set_ylim([float(yboundary[0]), float(yboundary[1])])
+                axes[j].set_zlim([float(zboundary[0]), float(zboundary[1])])
+                axes[j].scatter(trajectory[rollout_field][i][:, 0],
+                           trajectory[rollout_field][i][:, 1],
+                           trajectory[rollout_field][i][:, 2], s=1)
+                axes[j].view_init(elev=20., azim=i * 0.5)
+                axes[j].grid(True, which='both')
+
+        ani = animation.FuncAnimation(
+            fig, animate, frames=np.arange(0, num_steps, 3), interval=10)
+
+        ani.save(f'{FLAGS.rollout_path}/{FLAGS.output_filename}.gif', dpi=100, fps=30, writer='imagemagick')
+        print(f"Animation saved to: {FLAGS.rollout_path}/{FLAGS.output_filename}.gif")
+
 
 
 if __name__ == "__main__":
