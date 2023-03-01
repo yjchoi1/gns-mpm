7112773c8a8edee6ea97d78117022450ad577ea1
baagee <yjchoi9212@gmail.com>
2023-03-01 11:40:26 -0600

    Venv script for ls6 gpu

---
+++ build_venv_ls6.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+module reset
+
+# create env
+# ---------
+ml cuda/11.3
+ml cudnn
+ml nccl
+
+module load phdf5
+module load python3/3.9
+export LD_LIBRARY_PATH=/usr/lib64:$LD_LIBRARY_PATH
+
+python3 -m virtualenv venv-ls6-gpu
+source venv-ls6-gpu/bin/activate
+
+which python
+python -m pip install --upgrade pip
+pip install torch==1.12.1+cu113 torchvision==0.13.1+cu113 torchaudio==0.12.1 --extra-index-url https://download.pytorch.org/whl/cu113
+pip install pyg-lib torch-scatter torch-sparse torch-cluster torch-spline-conv torch-geometric -f https://data.pyg.org/whl/torch-1.12.1+cu113.html
+python -m pip install -r requirements.txt
+
+
+# test env
+# --------
+
+echo 'which python -> venv'
+which python
+
+echo 'test_pytorch.py -> random tensor'
+python gns-parallel/test/test_pytorch.py
+
+echo 'test_pytorch_cuda_gpu.py -> True if GPU'
+python gns-parallel/test/test_pytorch_cuda_gpu.py
+
+echo 'test_torch_geometric.py -> no retun if import sucessful'
+python gns-parallel/test/test_torch_geometric.py
+
+# Clean up
+# --------
+#deactivate
+#rm -r venv
