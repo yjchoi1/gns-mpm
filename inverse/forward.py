5373a26e3d4e7b4652c2ce63dd29ae9776361ba3
baagee <yjchoi9212@gmail.com>
2023-05-09 15:55:48 -0500

    inverse-forward run error computes only runout error

--- inverse/forward.py
+++ inverse/forward.py
@@ -63,7 +63,8 @@ def forward_rollout(
     predictions = torch.stack(predictions)
 
     # loss
-    loss = torch.mean((predictions[-1] - torch.from_numpy(target).to(device)) ** 2)
+    runout_distance = predictions[-1][:, 0].max()
+    loss = torch.mean((runout_distance - torch.tensor(target).to(device)) ** 2)
 
     output_dict = {
         'initial_positions': initial_positions.permute(1, 0, 2).cpu().numpy(),
@@ -86,47 +87,35 @@ def forward_rollout_autograd(
         target,
         device):
     """
-    Rolls out a trajectory by applying the model in sequence and -
-    compute loss between final runout of the column collapse at the edge and target value
-    Args:
-        simulator: Learned simulator
-        initial_positions: Torch tensor for initial position
-        particle_types: Torch tensor for particle_types
-        material_property: Torch tensor for particle_types, specifically `phi` in scalar
-        n_particles_per_example: Torch tensor for n_particles_per_example
-        nsteps: number of steps to predict
-        device:
 
-    Returns:
-        runout distance
     """
 
     current_positions = initial_positions
-    predictions = []
-    material_property_expended = material_property * torch.full((1, n_particles_per_example), 1).to(device)
+    # predictions = []
+    material_property_tensor = material_property * torch.full((1, n_particles_per_example), 1).to(device)
 
     print(f"Begin Rollout for {material_property.to(torch.float32)} rad...")
     for step in range(nsteps):
-        if step % 100 == 0:
+        print(f"Step {step}")
+        if step % 10 == 0:
             print(f"rollout step {step}/{nsteps} at phi {material_property.to(torch.float32)} rad...")
         # Get next position with shape (nnodes, dim)
         next_position = simulator.predict_positions(
             current_positions,
-            nparticles_per_example=[n_particles_per_example],
-            particle_types=particle_types,
-            material_property=material_property_expended
+            nparticles_per_example=[n_particles_per_example.detach().requires_grad_(False)],
+            particle_types=particle_types.detach().requires_grad_(False),
+            material_property=material_property_tensor
         )
-        predictions.append(next_position)
+        # predictions.append(next_position)
 
         # Shift `current_positions`, removing the oldest position in the sequence
         # and appending the next position at the end.
         current_positions = torch.cat(
             [current_positions[:, 1:], next_position[:, None, :]], dim=1)
 
-    # Predictions with shape (time, nnodes, dim)
-    predictions = torch.stack(predictions)
+    final_position = current_positions[:, -1, :]
 
     # loss
-    loss = torch.mean((predictions[-1] - torch.from_numpy(target).to(device)) ** 2)
+    loss = torch.mean((final_position[:, 0].max() - torch.tensor(target).to(device)) ** 2)
 
-    return loss
+    return loss, final_position.detach().cpu().numpy()
