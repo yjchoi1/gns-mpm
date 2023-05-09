6237343ba4a8f3af7c7d4a0aed0d93ce237e86e3
baagee <yjchoi9212@gmail.com>
2023-05-09 16:02:02 -0500

    ls6 venv for torch2.0

---
+++ build_venv_ls6_torch20.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+module reset
+
+# create env
+# ---------
+ml cuda/12.0
+ml cudnn
+ml nccl
+
+module load gcc/11.2.0
+module load intel/19.1.1
+module load intel/19.1.1  
+module load intel/19.1.1  
+module load intel/19.1.1  
+module load impi/19.0.9
+module load mvapich2-gdr/2.3.7
+module load mvapich2/2.3.7
+
+module load phdf5/1.10.4
+module load python3/3.9.7
+
+export LD_LIBRARY_PATH=/usr/lib64:$LD_LIBRARY_PATH
+
+python3 -m virtualenv venv-ls6-gpu-torch20
+source venv-ls6-gpu-torch20/bin/activate
+
+which python
+python -m pip install --upgrade pip
+pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+pip install pyg_lib torch_scatter torch_sparse torch_cluster torch_spline_conv -f https://data.pyg.org/whl/torch-2.0.0+cu118.html
+python -m pip install -r requirements.txt
+
+
+# test env
+# --------
+
+echo 'which python -> venv'
+which python
+
+echo 'test_pytorch.py -> random tensor'
+python gns-parallel/test/test_pytorch.py
+
+echo 'test_pytorch_cuda_gpu.py -> True if GPU'
+python gns-parallel/test/test_pytorch_cuda_gpu.py
+
+echo 'test_torch_geometric.py -> no retun if import sucessful'
+python gns-parallel/test/test_torch_geometric.py
+
+# Clean up
+# --------
+#deactivate
+#rm -r venv
---
+++ start_venv_ls6_torch20.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+module reset
+
+ml cuda/11.3
+ml cudnn
+ml nccl
+
+module load gcc/11.2.0
+module load intel/19.1.1
+module load intel/19.1.1  
+module load intel/19.1.1  
+module load intel/19.1.1  
+module load impi/19.0.9
+module load mvapich2-gdr/2.3.7
+module load mvapich2/2.3.7
+
+module load phdf5/1.10.4
+module load python3/3.9.7
+export LD_LIBRARY_PATH=/usr/lib64:$LD_LIBRARY_PATH
+cd gns
+source ../venv-ls6-gpu-torch20/bin/activate
+
+# test env
+# --------
+
+echo 'which python -> venv'
+which python
+
+echo 'test_pytorch.py -> random tensor'
+python test/test_pytorch.py
+
+echo 'test_pytorch_cuda_gpu.py -> True if GPU'
+python test/test_pytorch_cuda_gpu.py
+
+echo 'test_torch_geometric.py -> no retun if import sucessful'
+python test/test_torch_geometric.py
+
+# Clean up
+# --------
+#deactivate
+#rm -r venv
