aa15f7209fdfaff963da5fd06fdd0815eba0a76d
baagee <yjchoi9212@gmail.com>
2023-07-01 14:29:41 -0500

    Do running sum for statistics

--- utils/merge_npz_files.py
+++ utils/merge_npz_files.py
@@ -30,74 +30,90 @@ if dim == 3:
     running_sum = dict(velocity_x=0, velocity_y=0, velocity_z=0, acceleration_x=0, acceleration_y=0, acceleration_z=0)
     running_sumsq = dict(velocity_x=0, velocity_y=0, velocity_z=0, acceleration_x=0, acceleration_y=0, acceleration_z=0)
     running_count = dict(velocity_x=0, velocity_y=0, velocity_z=0, acceleration_x=0, acceleration_y=0, acceleration_z=0)
-aggregated_positions = []
-aggregated_velocities = []
-aggregated_accelerations = []
 data_names = []
 
+# for computing statistics
+cumulative_count = 0
+cumulative_sum_vel = np.zeros((1, dim))
+cumulative_sum_acc = np.zeros((1, dim))
+cumulative_sumsq_vel = np.zeros((1, dim))
+cumulative_sumsq_acc = np.zeros((1, dim))
+
+# iterate over each simulation
 for id in tqdm(data_tags, total=len(data_tags)):
     data_name = f"{data_case}{id}"
     data_names.append(data_name)
     npz_path = f"{mpm_dir}/{data_name}.npz"  # f"{mpm_dir}/{data_name}/{data_name}.npz"
     data = np.load(npz_path, allow_pickle=True)
-    for simulation_id, trajectory in data.items():
+    # get trajectory info
+    for simulation_id, trajectory in data.items():  # note, only one trajectory exists, so no need to iterate
         trajectories[f"simulation_trajectory_{id}"] = (trajectory)
 
-        # get positions for each mpm simulation
-        positions = trajectory[0]
-        array_shape = positions.shape
-        flattened_positions = np.reshape(positions, (-1, array_shape[-1]))
+    # get positions for each mpm simulation
+    positions = trajectory[0]
+    array_shape = positions.shape
+    flattened_positions = np.reshape(positions, (-1, array_shape[-1]))
 
-        # compute velocities using finite difference
-        # assume velocities before zero are equal to zero
-        velocities = np.empty_like(positions)
-        velocities[1:] = (positions[1:] - positions[:-1]) / dt_gns
-        velocities[0] = 0
-        flattened_velocities = np.reshape(velocities, (-1, array_shape[-1]))
+    # compute velocities using finite difference
+    # assume velocities before zero are equal to zero
+    velocities = np.empty_like(positions)
+    velocities[1:] = (positions[1:] - positions[:-1]) / dt_gns
+    velocities[0] = 0
+    flattened_velocities = np.reshape(velocities, (-1, array_shape[-1]))
 
-        # compute accelerations finite difference
-        # assume accelerations before zero are equal to zero
-        accelerations = np.empty_like(velocities)
-        accelerations[1:] = (velocities[1:] - velocities[:-1]) / dt_gns
-        accelerations[0] = 0
-        flattened_accelerations = np.reshape(accelerations, (-1, array_shape[-1]))
+    # compute accelerations finite difference
+    # assume accelerations before zero are equal to zero
+    accelerations = np.empty_like(velocities)
+    accelerations[1:] = (velocities[1:] - velocities[:-1]) / dt_gns
+    accelerations[0] = 0
+    flattened_accelerations = np.reshape(accelerations, (-1, array_shape[-1]))
 
-        # aggregate the arrays
-        aggregated_positions.append(flattened_positions)
-        aggregated_velocities.append(flattened_velocities)
-        aggregated_accelerations.append(flattened_accelerations)
+    # Compute statistics
+    cumulative_count += len(flattened_velocities)
+    # running sum
+    cumulative_sum_vel += np.sum(flattened_velocities, axis=0)
+    cumulative_sum_acc += np.sum(flattened_accelerations, axis=0)
+    # running sum squared
+    cumulative_sumsq_vel += np.sum(flattened_velocities**2, axis=0)
+    cumulative_sumsq_acc += np.sum(flattened_accelerations**2, axis=0)
+    # statistics for cumulative data
+    cumulative_mean_vel = cumulative_sum_vel / cumulative_count
+    cumulative_mean_acc = cumulative_sum_acc / cumulative_count
+    # cumulative_std_vel = np.sqrt(
+    #     (cumulative_sumsq_vel - cumulative_sum_vel ** 2 / cumulative_count) / (cumulative_count - 1))
+    # cumulative_std_acc = np.sqrt(
+    #     (cumulative_sumsq_acc - cumulative_sum_acc ** 2 / cumulative_count) / (cumulative_count - 1))
+    cumulative_std_vel = np.sqrt(
+        (cumulative_sumsq_vel/cumulative_count - (cumulative_sum_vel/cumulative_count)**2))
+    cumulative_std_acc = np.sqrt(
+        (cumulative_sumsq_acc/cumulative_count - (cumulative_sum_acc/cumulative_count)**2))
 
-# Concatenate the aggregated arrays
-concat_positions = np.concatenate(aggregated_positions)
-concat_velocities = np.concatenate(aggregated_velocities)
-concat_accelerations = np.concatenate(aggregated_accelerations)
-
-# Compute statistics
+# Store final statistics
 if dim == 2:
     statistics = {
-        "mean_velocity_x": float(np.mean(concat_velocities[:, 0])),
-        "mean_velocity_y": float(np.mean(concat_velocities[:, 1])),
-        "std_velocity_x": float(np.std(concat_velocities[:, 0])),
-        "std_velocity_y": float(np.std(concat_velocities[:, 1])),
-        "mean_accel_x": float(np.mean(concat_accelerations[:, 0])),
-        "mean_accel_y": float(np.mean(concat_accelerations[:, 1])),
-        "std_accel_x": float(np.std(concat_accelerations[:, 0])),
-        "std_accel_y": float(np.std(concat_accelerations[:, 1]))
+        "mean_velocity_x": float(cumulative_mean_vel[:, 0]),
+        "mean_velocity_y": float(cumulative_mean_vel[:, 1]),
+        "std_velocity_x": float(cumulative_std_vel[:, 0]),
+        "std_velocity_y": float(cumulative_std_vel[:, 1]),
+        "mean_accel_x": float(cumulative_mean_acc[:, 0]),
+        "mean_accel_y": float(cumulative_mean_acc[:, 1]),
+        "std_accel_x": float(cumulative_std_acc[:, 0]),
+        "std_accel_y": float(cumulative_std_acc[:, 1])
     }
 if dim == 3:
     statistics = {
-        "mean_velocity_x": float(np.mean(concat_velocities[:, 0])),
-        "mean_velocity_y": float(np.mean(concat_velocities[:, 1])),
-        "mean_velocity_z": float(np.mean(concat_velocities[:, 2])),
-        "std_velocity_x": float(np.std(concat_velocities[:, 0])),
-        "std_velocity_y": float(np.std(concat_velocities[:, 1])),
-        "std_velocity_z": float(np.std(concat_velocities[:, 2])),
-        "mean_accel_x": float(np.mean(concat_accelerations[:, 0])),
-        "mean_accel_y": float(np.mean(concat_accelerations[:, 1])),
-        "mean_accel_z": float(np.mean(concat_accelerations[:, 2])),
-        "std_accel_x": float(np.std(concat_accelerations[:, 0])),
-        "std_accel_y": float(np.std(concat_accelerations[:, 1])),
-        "std_accel_z": float(np.std(concat_accelerations[:, 2]))
+        "mean_velocity_x": float(cumulative_mean_vel[:, 0]),
+        "mean_velocity_y": float(cumulative_mean_vel[:, 1]),
+        "mean_velocity_z": float(cumulative_mean_vel[:, 2]),
+        "std_velocity_x": float(cumulative_std_vel[:, 0]),
+        "std_velocity_y": float(cumulative_std_vel[:, 1]),
+        "std_velocity_z": float(cumulative_std_vel[:, 2]),
+        "mean_accel_x": float(cumulative_mean_acc[:, 0]),
+        "mean_accel_y": float(cumulative_mean_acc[:, 1]),
+        "mean_accel_z": float(cumulative_mean_acc[:, 2]),
+        "std_accel_x": float(cumulative_std_acc[:, 0]),
+        "std_accel_y": float(cumulative_std_acc[:, 1]),
+        "std_accel_z": float(cumulative_std_acc[:, 2])
     }
 
 # Print statistics
