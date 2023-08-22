ee829bab15d0b35b99974ca1c5ea284da1ac29ec
baagee <yjchoi9212@gmail.com>
2023-08-22 13:49:24 -0500

    Improve render.py

--- meshnet/render.py
+++ meshnet/render.py
@@ -7,11 +7,14 @@ import matplotlib.tri as tri
 from mpl_toolkits.axes_grid1 import ImageGrid
 from matplotlib import animation
 from scipy.interpolate import griddata
+from utils import NodeType
 
 
 flags.DEFINE_string("rollout_dir", "/work2/08264/baagee/frontera/gns-meshnet-data/gns-data/rollouts/lbm-pipe/", help="Directory where rollout.pkl are located")
-flags.DEFINE_string("rollout_name", "rollout_0", help="Name of rollout `.pkl` file")
+flags.DEFINE_string("rollout_name", "rollout_2", help="Name of rollout `.pkl` file")
 flags.DEFINE_string("mesh_type", "quad", help="Mesh type, either `triangle` or `quad`")
+flags.DEFINE_integer("nnode_x", 161, help="Argument only for quad mesh render. Number of nodes for x-axis")
+flags.DEFINE_integer("nnode_y", 41, help="Argument only for quad mesh render. Number of nodes for y-axis")
 flags.DEFINE_integer("step_stride", 5, help="Stride of steps to skip.")
 FLAGS = flags.FLAGS
 
@@ -39,14 +42,27 @@ def render_gif_animation():
     if FLAGS.mesh_type == "triangle":
         triang = tri.Triangulation(result["node_coords"][0][:, 0], result["node_coords"][0][:, 1])
 
+
     # color
+    # TODO (yc): need to determine a single color bar parameter.
     vmin = np.concatenate(
         (result["predicted_rollout"][0][:, 0], result["ground_truth_rollout"][0][:, 0])).min()
     vmax = np.concatenate(
         (result["predicted_rollout"][0][:, 0], result["ground_truth_rollout"][0][:, 0])).max()
+    # Setting contour levels between min and max values of magnitudes
+    levels = np.linspace(velocity_result["ground_truth"].min(),
+                         velocity_result["ground_truth"].max(),
+                         100)
+
+    # Create masks for each node type
+    node_type = result["node_types"][0]
+    mask_normal = np.squeeze(node_type == NodeType.NORMAL)
+    mask_outflow = np.squeeze(node_type == NodeType.OUTFLOW)
+    mask_inflow = np.squeeze(node_type == NodeType.INFLOW)
+    mask_wall = np.squeeze(node_type == NodeType.WALL_BOUNDARY)
 
     # Init figures
-    fig = plt.figure(figsize=(9.75, 3))
+    fig = plt.figure(figsize=(7, 4))
 
     def animate(i):
         print(f"Render step {i}/{n_timesteps}")
@@ -54,7 +70,7 @@ def render_gif_animation():
         fig.clear()
         grid = ImageGrid(fig, 111,
                          nrows_ncols=(2, 1),
-                         axes_pad=0.15,
+                         axes_pad=0.3,
                          share_all=True,
                          cbar_location="right",
                          cbar_mode="single",
@@ -63,44 +79,40 @@ def render_gif_animation():
 
         if FLAGS.mesh_type == "triangle":
             for j, (sim, vel) in enumerate(velocity_result.items()):
-                grid[j].triplot(triang, 'o-', color='k', ms=0.5, lw=0.3)
+                grid[j].triplot(triang, '-', color='k', ms=0.5, lw=2.0)
                 handle = grid[j].tripcolor(triang, vel[i], vmax=vmax, vmin=vmin)
-                fig.colorbar(handle, cax=grid.cbar_axes[0])
+                cbar = fig.colorbar(handle, cax=grid.cbar_axes[0])
+                cbar.set_label("Velocity (m/s)", rotation=270, labelpad=10)
                 fig.suptitle(f"{i}/{n_timesteps}")
-                grid[j].set_title(sim)
+                grid[j].set_title(sim, pad=3)
+                grid[j].set_xlabel("x (m)")
+                grid[j].set_ylabel("y (m)")
+
         if FLAGS.mesh_type == "quad":
             for j, (sim, vel) in enumerate(velocity_result.items()):
-
-                # X, Y = np.meshgrid(result["node_coords"][0][:, 0], result["node_coords"][0][:, 1])
-                # # Determine the number of unique x and y values
-                # nx = len(result["node_coords"][0][:, 0])
-                # ny = len(result["node_coords"][0][:, 1])
-                # # Reshape the velocity array
-                # Z = vel[i].reshape((nx, ny))
-
-                # create a grid of points on which to interpolate
-                grid_x, grid_y = np.mgrid[
-                                 min(result["node_coords"][0][:, 0]):max(result["node_coords"][0][:, 0]):100j,
-                                 min(result["node_coords"][0][:, 1]):max(result["node_coords"][0][:, 1]):100j]
-
-                # interpolate the velocities onto this grid
-                grid_velocity = griddata(result["node_coords"][0], vel[i], (grid_x, grid_y), method='cubic')
+                # Reshape data to fit my lbm grid structure. Current grid: (160 * 40)
+                vel_grid = vel[i].reshape(FLAGS.nnode_y, FLAGS.nnode_x)
+                x_grid = result["node_coords"][0][:, 0].reshape(FLAGS.nnode_y, FLAGS.nnode_x)
+                y_grid = result["node_coords"][0][:, 1].reshape(FLAGS.nnode_y, FLAGS.nnode_x)
 
                 # make the contour plot
-                handle = grid[j].contourf(grid_x, grid_y, grid_velocity, 50, vmax=vmax, vmin=vmin, cmap='viridis')
-                fig.colorbar(handle, cax=grid.cbar_axes[0])
+                velocity_contour = grid[j].contourf(x_grid, y_grid, vel_grid, 50, cmap='viridis', levels=levels)
+                # # Scatter plot for each node type with different color
+                # grid[j].scatter(result["node_coords"][0][mask_normal, 0],
+                #             result["node_coords"][0][mask_normal, 1], color='blue', s=1, label='Normal nodes')
+                # grid[j].scatter(result["node_coords"][0][mask_inflow, 0],
+                #             result["node_coords"][0][mask_inflow, 1], color='orange', s=10, label='Inlet nodes')
+                # grid[j].scatter(result["node_coords"][0][mask_outflow, 0],
+                #             result["node_coords"][0][mask_outflow, 1], color='green', s=10, label='Outlet nodes')
+                # grid[j].scatter(result["node_coords"][0][mask_wall, 0],
+                #             result["node_coords"][0][mask_wall, 1], color='red', s=3, label='Wall boundary')
+                cbar = fig.colorbar(velocity_contour, cax=grid.cbar_axes[0])
+                cbar.set_label("Velocity (m/s)", rotation=270, labelpad=10)
                 fig.suptitle(f"{i}/{n_timesteps}")
-                grid[j].set_title(sim)
-
-                # handle = grid[j].contourf(X, Y, Z, 50, vmax=vmax, vmin=vmin, cmap='viridis')
-                # fig.colorbar(handle, cax=grid.cbar_axes[0])
-                # grid[j].set_title(sim)
-                # plt.xlabel('X')
-                # plt.ylabel('Y')
-
-
-
-
+                grid[j].set_aspect('equal')
+                grid[j].set_title(sim, pad=3)
+                grid[j].set_xlabel("x (m)")
+                grid[j].set_ylabel("y (m)")
 
     # Creat animation
     ani = animation.FuncAnimation(
